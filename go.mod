2076e974ad3a5bbc5ee13a4a5d77e585a6d8e36d
Aaron Tatum <actatum@umich.edu>
2023-12-05 15:51:43 -0500

    upgrade to go1.21 and use slog for the logger middleware

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/actatum/stormrpc
 
-go 1.20
+go 1.21
 
 require (
 	github.com/google/uuid v1.3.0
@@ -10,12 +10,10 @@ require (
 	go.opentelemetry.io/otel v1.14.0
 	go.opentelemetry.io/otel/sdk v1.7.0
 	go.opentelemetry.io/otel/trace v1.14.0
-	go.uber.org/zap v1.24.0
 	google.golang.org/protobuf v1.29.0
 )
 
 require (
-	github.com/benbjohnson/clock v1.1.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -25,8 +23,6 @@ require (
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
-	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
--- middleware/logging.go
+++ middleware/logging.go
@@ -3,16 +3,16 @@ package middleware
 
 import (
 	"context"
+	"log/slog"
 	"time"
 
 	"github.com/actatum/stormrpc"
 	"go.opentelemetry.io/otel/trace"
-	"go.uber.org/zap"
 )
 
 // Logger logs request scoped information such as request id, trace information, and request duration.
 // This middleware should be applied after RequestID, and Tracing.
-func Logger(l *zap.Logger) func(next stormrpc.HandlerFunc) stormrpc.HandlerFunc {
+func Logger(l *slog.Logger) func(next stormrpc.HandlerFunc) stormrpc.HandlerFunc {
 	return func(next stormrpc.HandlerFunc) stormrpc.HandlerFunc {
 		return func(ctx context.Context, r stormrpc.Request) stormrpc.Response {
 			span := trace.SpanFromContext(ctx)
@@ -22,20 +22,33 @@ func Logger(l *zap.Logger) func(next stormrpc.HandlerFunc) stormrpc.HandlerFunc 
 
 			resp := next(ctx, r)
 
-			fields := []zap.Field{
-				zap.String("id", id),
-				zap.String("trace_id", span.SpanContext().TraceID().String()),
-				zap.String("span_id", span.SpanContext().SpanID().String()),
-				zap.String("duration", time.Since(start).String()),
+			attrs := make([]slog.Attr, 0)
+
+			level := slog.LevelInfo
+			msg := "Success"
+			if resp.Err != nil {
+				msg = "Server Error"
+				level = slog.LevelError
+				code := stormrpc.CodeFromErr(resp.Err)
+				attrs = append(attrs, slog.Group(
+					"error",
+					slog.String("message", resp.Err.Error()),
+					slog.String("code", code.String()),
+				))
 			}
 
-			if resp.Err != nil {
-				code := stormrpc.CodeFromErr(resp.Err)
-				fields = append(fields, zap.String("code", code.String()))
-				l.Error("Server Error", fields...)
-			} else {
-				l.Info("Success", fields...)
-			}
+			attrs = append(attrs, slog.Group("request",
+				slog.String("id", id),
+				slog.String("trace_id", span.SpanContext().TraceID().String()),
+				slog.String("duration", time.Since(start).String()),
+			))
+
+			l.LogAttrs(
+				ctx,
+				level,
+				msg,
+				attrs...,
+			)
 
 			return resp
 		}
